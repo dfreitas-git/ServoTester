0d9548665f6b0eaa6925d3c85f5b02d292863931
dfreitas-git <dfreitas.hh@gmail.com>
2025-01-19 12:43:32 -0800

    New schematic/code with backlight and servo-sweep-time control.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <Arduino.h>
 
-// For Nokia OLED display
-//#include <Adafruit_GFX.h>
-//#include <Adafruit_PCD8544.h>
+/* ServoTester
+  
+   Up to four servos can be tested at a time.  All four servos can be driven simultaneously (with the same input range/mode) 
+   or each servo can have a separate minimum angle, maximum angle and automatic sweep or manual move mode.
+  
+   Two level menu is used.  The top level menu shows servo-0/1/2/3 and one called "A" (for all).  Rotating the active line
+   indicator (an "=" in the left most column) to the desired servo and clicking the rotary switch puts you into the setting
+   mode for that servo.  From there you can set the min-angle, max-angle, mode (POT - for potentiometer, or SWP - for auto-
+   sweep), and the current angle.  Select the desired line and click to enter edit mode.  In edit mode you turn the rotary
+   knob to increase/decrease the angle or select the different modes.  Select "Back" at the bottom of the menu to return to 
+   the top level menu.
+  
+   Selecting the ServoA line puts you into a mode where the setting apply to all the servos at once (overriding any settings
+   that were set for each individual servo).  It allows you to set or sweep all servos with the same settings.
+  
+   DLF 1/18/2025
+*/
+
 #include <RotaryEncoder.h>
 #include <Servo.h>
 #include <U8g2lib.h>
@@ -17,15 +32,14 @@ enum servoMode { POT, SWP };  // Either controlled with a potentiometer (POT) or
 enum rotary { LINE, MODE, CURA, MINA, MAXA };  // What the rotary knob is controlling
 
 // Constants
-#define OLED_REFRESH_TIME  100   // Time (ms) between OLED screen updates
-#define SERVO_MOVE_TIME    500   // Time (ms) between servo sweep steps
+#define OLED_REFRESH_TIME  60    // Time (ms) between OLED screen updates
 boolean PRESSED = false;
 
 // U8G2 for PCD8544 with software SPI
-//U8G2_PCD8544_84X48_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 12, /* data=*/ 11, /* cs=*/ 7, /* dc=*/ 8, /* reset=*/ 4);
 U8X8_PCD8544_84X48_4W_SW_SPI u8x8(/* clock=*/ 12, /* data=*/ 11, /* cs=*/ 7, /* dc=*/ 8, /* reset=*/ 4);
 
-char str[11];  // Array to hold sprintf strings
+// Array to hold sprintf strings
+char str[11];  
 char modeStr[4];
 char selChar[2];
 
@@ -41,24 +55,25 @@ int encoderCurPosition = 0;
 
 // Servos
 #define NUM_SERVOS 5
-int servoPinArr[NUM_SERVOS] = {10,9,6,5,-1};
-int servoCurAngleArr[NUM_SERVOS] = {90,90,90,90,90};
-int servoMinAngleArr[NUM_SERVOS] = {0,0,0,0,-20};
-int servoMaxAngleArr[NUM_SERVOS] = {180,180,180,180,200};
-servoMode servoModeArr[NUM_SERVOS] = {POT,POT,POT,POT,POT};  // modes are POT or SWP  (potentiometer or sweep)
-Servo servoInstance[NUM_SERVOS-1]; // Create the servo objects for the actual servos (servo5 means ALL servos).
+#define SERVO_SWEEP_SPEED_PIN A2       // So we can control how fast the servos sweep
+int servoPinArr[NUM_SERVOS] = {10,9,6,5,-1};  // Fifth entry is just a dummy placeholder for the ALL-SERVOS settings.  We don't assign a real servo to it.
+int servoCurAngleArr[NUM_SERVOS] = {90,90,90,90,90};        // Current angle setting for each servo
+int servoMinAngleArr[NUM_SERVOS] = {0,0,0,0,-20};           // Minimum angle limit for each servo
+int servoMaxAngleArr[NUM_SERVOS] = {180,180,180,180,200};   // Maximum angle limit for each servo
+servoMode servoModeArr[NUM_SERVOS] = {POT,POT,POT,POT,POT}; // modes are POT or SWP  (potentiometer or sweep-auto)
+Servo servoInstance[NUM_SERVOS-1]; // Create the servo objects for the actual servos 
 
 // Menu Vars
 uint8_t menuLevel = 0;
-uint8_t level0SelectRow = 0;
-uint8_t level1SelectRow = 0;
+uint8_t level0SelectRow = 0;     // The top level row that is being selected to use
+uint8_t level1SelectRow = 0;     // The level-1 menu row that is beign selected
 uint8_t NUM_LEVEL0_ITEMS = 5;
 uint8_t NUM_LEVEL1_ITEMS = 5;
-boolean editing = false;   // Set true when editing data in a menu row
+boolean editing = false;         // Set true when editing data in a menu row
 
 // Misc Globals
-int sweepAngle = 0;  // Where we are at in the sweep
-int sweepDelta = 5;  // Number of degrees we move on each step during a sweep
+int sweepAngle = 0;                   // Where we are at in the sweep
+int sweepDelta = 1;                   // Number of degrees we move on each step during a sweep
 boolean sweepAngleRising = true;      // CW or CCW rotation
 unsigned long lastDisplayUpdate = 0;  // last OLED update
 unsigned long lastSweepUpdate = 0;    // last servo step move
@@ -77,13 +92,13 @@ void setup() {
    while(!Serial) { }
    delay(1000);
 
+   // Initialize the servos
    for(int i = 0; i < NUM_SERVOS-1; i++) {
       servoInstance[i].attach(servoPinArr[i]);  
    }
 
-   // Set up the oled
+   // Set up the oled.  This u8x8 is a text only library (no graphics)
    u8x8.begin();
-   //u8g2.begin();
 
    // Initialize the rotary encoder.  Enable the Arduino builtin pullup resistors.
    pinMode(ENC_SW, INPUT_PULLUP);
@@ -109,7 +124,12 @@ void loop() {
 
    encoderCurPosition = encoder->getPosition();
 
-   // Limit the encoder count range depending on which menu we are in (different menus have different number of rows)
+   // Set the speed that the servos will sweep
+    // Will be in ms as we'll use the raw 0-1023 reading with millis() later
+   uint16_t servoSweepSpeed = analogRead(SERVO_SWEEP_SPEED_PIN); 
+
+   // Limit the encoder count range depending on which menu/mode we are in (different menus have different number of choices)
+   // This lets us wrap the menu selector from max back to min when rotating the knob
    if(menuLevel == 0) {
       encoderCurPosition = getEncoderPosition(encoderCurPosition, 0, NUM_LEVEL0_ITEMS-1);
       level0SelectRow = encoderCurPosition;
@@ -145,6 +165,7 @@ void loop() {
       while(checkSwitch(ENC_SW) == PRESSED) {
          delay(1);
       }
+      // Top level menu
       if(menuLevel == 0) {
          level0SelectRow = encoderCurPosition;
          level1SelectRow = 0;
@@ -152,6 +173,7 @@ void loop() {
          menuLevel = 1;
          rotaryControl = LINE;
  
+      // Individual servo sub-menu
       } else if(menuLevel == 1 && level1SelectRow == 0 && !editing) {   // This is the "mode" line
          rotaryControl = MODE;
          editing = true;
@@ -206,11 +228,16 @@ void loop() {
          for(int i = 0; i < NUM_SERVOS; i++) {
             if(servoModeArr[i] == POT) { strcpy(modeStr, "POT"); }
             if(servoModeArr[i] == SWP) { strcpy(modeStr, "SWP"); }
+
+            // Indicate the selected row by placing an "=" in the left column.  That will be 
+            // the servo whos setup we enter if we click the rotary switch
             if(level0SelectRow == i ) {
                strcpy(selChar,"=");
             } else {
                strcpy(selChar," ");
             }
+            // Name "servo4" as servoA  for ALL.  When we select this servo, 
+            // its setup page will apply to all the servos at once.
             if(i == 4) {
                sprintf(str,"%sA %s %d", selChar, modeStr, servoCurAngleArr[i]);
             } else {
@@ -219,6 +246,10 @@ void loop() {
             u8x8.drawString(0,i+1,str);
          }
       }
+      // This is the servo setup sub-menu.  Here each entry can be edited to change
+      // mode/current-angle,min-angle, and max-angle for the servo.
+      // When we click on an entry, the font is changed so we can see we are in edit mode.
+      // To exit edit mode, click again.
       if(menuLevel == 1) {
          // Menu Header
          u8x8.clearDisplay();
@@ -277,9 +308,10 @@ void loop() {
 
    // Now go move the servo(s)
    for(int i = 0; i < NUM_SERVOS-1; i++) {
+      // row 4 means all servos driven with the "servoA" settings
       if(level0SelectRow == 4) {
-         if(servoModeArr[4] == POT) {   // row 4 means all servos driven with the "servo4" settings
-            servoInstance[i].write(servoCurAngleArr[4]);        // sets the servo position 
+         if(servoModeArr[4] == POT) {  
+            servoInstance[i].write(servoCurAngleArr[4]);     // sets the servo position 
          }
       } else {
          if(servoModeArr[i] == POT) {
@@ -289,14 +321,15 @@ void loop() {
    }
 
    // For any servos being automatically swept
-   if(millis() - lastSweepUpdate > SERVO_MOVE_TIME) {
+   if(millis() - lastSweepUpdate > servoSweepSpeed) {
       lastSweepUpdate = millis();
       int index;
       for(int i = 0; i < NUM_SERVOS-1; i++) {
             index = i;
-         if(level0SelectRow == 4) {   // row 4 means all servos driven with the "servo4" settings
+         if(level0SelectRow == 4) {   // row 4 means all servos driven with the "servoA" settings
             index = 4;
          }
+         // Calculate the next servo position angle.  Ramp up then back down and repeat.
          if(!sweepAngleRising && sweepAngle <= servoMinAngleArr[index]) {
             sweepAngleRising = true;
             sweepAngle = servoMinAngleArr[index];
@@ -311,7 +344,7 @@ void loop() {
          if(!sweepAngleRising && sweepAngle > servoMinAngleArr[index]) {
             sweepAngle -= sweepDelta;
          }
-         if(level0SelectRow == 4 && servoModeArr[4] == SWP) {   // row 4 means all servos driven with the "servo4" settings
+         if(level0SelectRow == 4 && servoModeArr[4] == SWP) {   // row 4 means all servos driven with the "servoA" settings
             servoInstance[i].write(sweepAngle);                 // move the indexed servo to the new position
             servoCurAngleArr[4] = sweepAngle;
          } else {
